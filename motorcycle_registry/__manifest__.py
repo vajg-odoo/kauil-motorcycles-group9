ca3d2abdeb378f585342d84e357bddf0131b6156
“fsrs-odoo” <fsrs@odoo.com>
2023-06-01 17:33:01 +0000

    [ADD] Motorcycle Registry Module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Motorcycle Registry",
+    
+    "summary": "Manage Registration of Motorcycles",
+    
+    "description": """
+    Motorcycle Registry
+====================
+This Module is used to keep track of the Motorcycle Reistration and Ownership of each motorcycled of the brand.
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["base"],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
