39b6e4415a6554f553a9af0b54bfec7810dbd2ba
“fsrs-odoo” <fsrs@odoo.com>
2023-06-02 21:42:07 +0000

    [ADD] Relational, Related and Computed Fields. Add VIN Regex

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -15,13 +15,23 @@ class MotorcycleRegistry(models.Model):
     
     registry_number = fields.Char('Registry Number', copy=False, required=True, readonly=True, default='MRN0000')
     vin = fields.Char(string='VIN', required=True)
-    first_name = fields.Char(string='First Name', required=True)
-    last_name = fields.Char(string='Last Name', required=True)
+    # first_name = fields.Char(string='First Name', required=True)
+    # last_name = fields.Char(string='Last Name', required=True)
     picture = fields.Image(string='Photograph')
     current_mileage = fields.Float(string='Current Mileage')
     license_plate = fields.Char(string='License Plate Number')
     certificate_title = fields.Binary(string='Certificate of Title')
     registry_date = fields.Date(string='Registration Date')
+
+    # Owner fields
+    owner_id = fields.Many2one(comodel_name='res.partner', ondelete='restrict')
+    owner_phone = fields.Char(related='owner_id.phone')
+    owner_email = fields.Char(related='owner_id.email')
+    
+    # Vehicles fields
+    brand = fields.Char(compute='_compute_from_vin')
+    make = fields.Char(compute='_compute_from_vin')
+    model = fields.Char(compute='_compute_from_vin')
 
         
     @api.constrains('license_plate')
@@ -32,6 +42,15 @@ class MotorcycleRegistry(models.Model):
                 match = re.match(pattern, registry.license_plate)
                 if not match:
                     raise ValidationError('Invalid License Plate')
+ 
+    @api.constrains('vin')
+    def _check_vin_pattern(self):
+        pattern = '^[A-Z]{4}\d{2}[A-Z0-9]{2}\d{6}$'
+        for registry in self:
+            if registry.vin:
+                match = re.match(pattern, registry.vin)
+                if not match:
+                    raise ValidationError('Invalid VIN')
                     
     @api.model_create_multi
     def create(self, vals_list):
@@ -39,3 +58,10 @@ class MotorcycleRegistry(models.Model):
             if vals.get('registry_number', ('MRN0000')) == ('MRN0000'):
                 vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry.number')
         return super().create(vals_list)
+    
+    @api.depends('vin')
+    def _compute_from_vin(self):
+        for registry in self:
+            registry.brand = registry.vin[:2]
+            registry.make = registry.vin[2:4]
+            registry.model = registry.vin[4:6]
