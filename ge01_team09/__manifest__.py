e876e23410d757ac43e27ec6374ac64aa69ef984
Varasiddhi Jayasuryaa Govindraj <vajg@odoo.com>
2023-06-22 09:38:03 -0400

    Completed GE01

---
+++ ge01_team09/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": "Group Task Team 09",
+    
+    "summary": "Provide VIN as serial number",
+    
+    "description": """
+    Motorcycle Registry
+====================
+This Module is used to keep track of the Motorcycle Reistration and Ownership of each motorcycled of the brand.
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry"],
+    
+    "data": [
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
