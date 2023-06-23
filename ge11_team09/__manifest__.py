945bdb95aafc82d2780f2a3d2441b561be2f72fd
Varasiddhi Jayasuryaa Govindraj <vajg@odoo.com>
2023-06-23 13:37:36 -0400

    Completed GE11

---
+++ ge11_team09/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": "Group Task 11 Team 09",
+    
+    "summary": "Automatic Dealer Redirection",
+    
+    "description": """
+    Motorcycle Registry
+====================
+This Module is used to for Automatic Dealer Redirection.
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry" ,"sale_stock"],
+    
+    "data": [
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
---
+++ ge11_team09/models/sale_order.py
@@ -0,0 +1,31 @@
+from odoo import api, fields, models
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    
+    warehouse_id = fields.Many2one(required = True, stored = True, copy = True, compute = '_compute_warehouse_id', readonly=True)
+    
+    @api.depends("partner_shipping_id")
+    def _compute_warehouse_id(self):
+        customer_address = self._compute_geolocations(self.partner_shipping_id)
+        print(self)
+        for record in self:
+            print(record, record.warehouse_id)
+            warehouses = self.env['stock.warehouse'].search([]).filtered(lambda r: r.code != "WH")
+            closest_warehouse = sorted(warehouses, key = lambda wh: self._compute_distance_to_customer(wh, customer_address))[0]
+            self.warehouse_id = closest_warehouse
+            
+    def _compute_distance_to_customer(self, warehouse, customer_location):
+        warehouse_location = self._compute_geolocations(warehouse.partner_id)
+        distance_to_customer = geodesic(warehouse_location, customer_location).miles
+        
+        return distance_to_customer
+        
+    
+    def _compute_geolocations(self, res_partner):
+        geolocator = Nominatim(user_agent="http")
+        geolocation =  geolocator.geocode(res_partner.contact_address_complete)
+        return geolocation.latitude, geolocation.longitude
+        
