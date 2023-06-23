76e4bcb84082e286fca344556d38f32da66b09b5
liya-odoo <liya@odoo.com>
2023-06-23 09:30:59 -0400

    add string for name stock_ids and stock_id

--- ge_2_9/models/motorcycle_registry.py
+++ ge_2_9/models/motorcycle_registry.py
@@ -6,15 +6,17 @@ class MotorcycleRegistry(models.Model):
 
     # adding the one2many field
     # adding contrain on stock_ids, so it only allow one record to exist
-    stock_ids = fields.One2many(comodel_name='stock.lot', inverse_name = 'registry_id')
+    stock_ids = fields.One2many(comodel_name='stock.lot', inverse_name = 'registry_id', 
+                                string='Stock Ids' # Distinct label)
 
     # for here, we grab 'stock_ids' and using it to compute our result and show it to users
-    stock_id = fields.Many2one(comodel_name='stock.lot', ondelete='restrict', compute="_compute_from_stock_ids")
+    stock_id = fields.Many2one(comodel_name='stock.lot', ondelete='restrict', 
+                               compute="_compute_from_stock_ids", string='Primary Stock' # Distinct label)
 
 
     @api.constrains('stock_ids')
-    def _one_stock_ids(self):
-        self.ensure_one()
+    def _one_stock_ids(self):   
+        self.ensure_one()   
         # adding logic that only allow one stock_ids exist
         # so user can not create one more
         for registry_entry in self:
