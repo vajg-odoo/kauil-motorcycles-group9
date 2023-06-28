999698b4901d5526a701a1fa76eeb91f839eeea2
Philip Snyder <phsn@odoo.com>
2023-06-28 11:57:58 -0400

    not finished ge5

---
+++ ge_5_9/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "ge5",
+    
+    "summary": "Motorcycle Mileage Web Snippet",
+    
+    "description": """ge5.
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["ge_2_9"],
+    
+    "data": [
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": False,
+    "installable": True,
+}
